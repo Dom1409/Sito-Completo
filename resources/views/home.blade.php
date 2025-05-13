daa189970546533d2db5608665efc9bb3df80518
Domenico <sltdnc00p14d960d@studium.unict.it>
2025-05-13 19:37:54 +0200

    Modifica la view home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script>
-    const BASE_URL = window.location.origin + '/';
-
+    const BASE_URL="{{ url('/') }}/";
     </script>
     <link href="{{ url ('//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css') }}" rel="stylesheet">
     <script src='{{ url("js/home.js")}}' defer="true"></script>
