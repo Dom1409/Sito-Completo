ec39040cbe8aa7be91a138bd476c5096938044ba
Domenico <sltdnc00p14d960d@studium.unict.it>
2025-05-13 19:52:44 +0200

    Modifica CollectionController

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -37,7 +37,7 @@ class CollectionController extends BaseController
            curl_setopt($ch, CURLOPT_URL, $url);
            # Setto che voglio ritornato il valore, anziché un boolean (default)
            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+          // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
            # Eseguo la richiesta all'URL
            $res = curl_exec($ch);
            //decodifico il json e lo assegno ad una variabile
