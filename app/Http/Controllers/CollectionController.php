f7e5eb105a67df07ad9b50925de6cbafadeee56d
Domenico <sltdnc00p14d960d@studium.unict.it>
2025-05-15 02:27:52 +0200

    ultimo change

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -43,59 +43,9 @@ if ($res === false) {
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-dd('HTTP Status Code: ' . $httpCode, 'Risposta: ' . $res);
 
 }
 
-    
-    /*{
-            // Controllo accesso
-            if (!Session::get('user_id')) {
-                return redirect('index');
-            }
-        
-            $url='https://www.cheapshark.com/api/1.0/games?title='.$lettera;
-                
-            # Creo il CURL handle per l'URL selezionato
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            # Setto che voglio ritornato il valore, anziché un boolean (default)
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 30); // 30 secondi di timeout
-
-            # Eseguo la richiesta all'URL
-            $res = curl_exec($ch);
-            Log::info('Risposta cURL: ' . $res); 
-            if ($res === false) {
-    Log::error('Errore cURL: ' . curl_error($ch));
-    return response()->json(['errore durante la chiamata' => curl_error($ch)], 500);
-}
-
-            //decodifico il json e lo assegno ad una variabile
-            $json = json_decode($res, true);
-            if ($json === null) {
-    Log::error('Errore JSON: ' . json_last_error_msg());
-    return response()->json(['errore json' => json_last_error_msg()], 500);
-}
-
-            # Libero le risorse
-            if (curl_errno($ch)) {
-                 Log::error('Errore cURL: ' . curl_error($ch));
-                return response()->json(['errore durante la chiamata' => curl_error($ch)], 500);
-            }
-            curl_close($ch);
-            
-
-            if ($json === null) {
-                Log::error('Errore JSON vuoto: ' . json_last_error_msg());
-                return response()->json(['errore json vuoto' => json_last_error_msg()], 500);
-            }
-        
-            //impachetto tutto su un json e lo mando alla richiesta fetch fatta da javascript
-            return response()->json($json);
-    }*/
-        
 // funzione che restituisce la view della descrizione del gioco,
     //in caso non si ha la sessione dell'utente rimanda alla schermata di login
 
