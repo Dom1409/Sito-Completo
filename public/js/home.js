c8632d153794196c66cc79a99444eca03c42f9ad
Domenico <sltdnc00p14d960d@studium.unict.it>
2025-05-15 02:24:23 +0200

    altro change

--- public/js/home.js
+++ public/js/home.js
@@ -91,23 +91,8 @@ function onjson(json){
   
     console.log(lettera);
     
-  const fetchApi = async () => {
-  const response = await fetch(BASE_URL + 'collection/list/' + lettera);
-  if (!response.ok) {
-    console.error('Errore:', response.status, response.statusText);
-    return 'Errore';
-  }
-  return await response.json();
-}
-
-
-    avvia();
-    
-    
-   async function avvia() {
-      const data = await fetchApi();
-      onjson(data);
-    }
+    fetch('https://www.cheapshark.com/api/1.0/games?title=' + lettera)
+        .then(onResponce).then(onjson);
    
   
     
